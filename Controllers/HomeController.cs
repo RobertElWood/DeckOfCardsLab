912bd190fb81d3c589d2b0e7a25c1363739c02cd
Robert Woods <robert.woods@fulbrightmail.org>
2022-11-02 08:54:43 -0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using DeckofCardsAPILab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DeckofCardsAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        DOCAPIDAL api = new DOCAPIDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            NewDeck n = api.CreateDeck();
+            return View(n);
+        }
+
+        public IActionResult ShuffleDeckView(string deckId)
+        {
+            ShuffledDeck s = api.ShuffleDeck(deckId);
+            return View(s);
+        }
+
+        public IActionResult ReShuffleView(string deckId)
+        {
+            ShuffledDeck n = api.ReShuffleDeck(deckId);
+            return View(n);
+        }
+
+        public IActionResult Index2 (string deckId)
+        {
+            ShuffledDeck d = api.ReShuffleDeck(deckId);
+            return View(d);
+        }
+
+        public IActionResult DrawCardsView(string deckId, int count)
+        {
+            DrawACard d = api.DrawCards(deckId, count);
+            return View(d);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/DOCAPIDAL.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using RestSharp;
+using System.Runtime.CompilerServices;
+
+namespace DeckofCardsAPILab.Models
+{
+    public class DOCAPIDAL
+    {
+        //Creates a brand new deck, can be used to start over.
+        public NewDeck CreateDeck()
+        {
+            var client = new RestClient($"https://deckofcardsapi.com/api/deck/new/");
+            var request = new RestRequest();
+            var response = client.GetAsync<NewDeck>(request);
+            NewDeck d = response.Result;
+            return d;
+        }
+
+        //Shuffles deck at given ID of NewDeck object used as parameter.
+        public ShuffledDeck ShuffleDeck(string deckId)
+        {
+            var client = new RestClient($"https://deckofcardsapi.com/api/deck/{deckId}/shuffle/");
+            var request = new RestRequest();
+            var response = client.GetAsync<ShuffledDeck>(request);
+            ShuffledDeck e = response.Result;
+            return e;
+        }
+
+        //Not sure if I'll need a call tos reshuffle, but included just in case
+        public ShuffledDeck ReShuffleDeck(string deckId)
+        {
+            var client = new RestClient($"https://deckofcardsapi.com/api/deck/{deckId}/shuffle/?remaining=true");
+            var request = new RestRequest();
+            var response = client.GetAsync<ShuffledDeck>(request);
+            ShuffledDeck r = response.Result;
+            return r;
+        }
+
+        //Draws the number of cards specified from the shuffled deck.
+        public DrawACard DrawCards(string deckId, int count)
+        {
+            var client = new RestClient($"https://deckofcardsapi.com/api/deck/{deckId}/draw/?count={count}");
+            var request = new RestRequest();
+            var response = client.GetAsync<DrawACard>(request);
+            DrawACard d = response.Result;
+            return d;
+        }
+
+        //Keeps cards from your hand, removing them from the shuffled deck.
+        public AddToPiles KeepCards(string deckId, string cards)
+        {
+            var client = new RestClient($"https://deckofcardsapi.com/api/deck/{deckId}/pile/hand/add/?cards={cards}");
+            var request = new RestRequest();
+            var response = client.GetAsync<AddToPiles>(request);
+            AddToPiles d = response.Result;
+            return d;
+        }
+    }
+}
